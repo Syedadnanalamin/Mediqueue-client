17572cf24fcccc4cf7959fd55a7d52404cb6165e
syedadnanalamin <syedadnanhuda64@gmail.com>
2026-06-25 18:26:10 +0600

    complete tutor page

---
+++ src/app/tutors/page.jsx
@@ -0,0 +1,22 @@
+import Card from "@/Components/Homepage/AvailableTutors/Card";
+import { AlltutorsData } from "@/lib/api";
+
+
+const TutorsPage = async () => {
+    const Tutors = await AlltutorsData();
+
+
+    return (
+        <div className='w-full h-[700px] mt-2'>
+            <h1 className='text-3xl font-bold text-center '>Available Tutors</h1>
+
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 mx-auto w-[70%] gap-2 mt-5'>
+                {Tutors.map((tutor) => <Card key={tutor._id} tutor={tutor}></Card>)}
+
+            </div>
+        </div>
+    );
+};
+
+export default TutorsPage
+    ;
--- src/lib/api.js
+++ src/lib/api.js
@@ -3,3 +3,13 @@ export const TutorsAvaiable = async () => {
     const res = await fetch("http://localhost:8080/");
     return res.json();
 }
+export const AlltutorsData = async () => {
+
+    const res = await fetch("http://localhost:8080/tutors");
+    return res.json();
+}
+
+export const specificTutor = async (id) => {
+
+    const res = await fetch(`http://localhost:8080/tutors/details/${id}`);
+}
